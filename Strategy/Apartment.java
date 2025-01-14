ede094d84c4714a8aa2ced788ecdfbf8b4d73bc1
CSCE-JerardA <jerardaustin4@gmail.com>
2025-01-14 15:11:19 -0500

    adding apartment class

---
+++ Strategy/Apartment.java
@@ -0,0 +1,22 @@
+package Strategy;
+
+/*
+ * Written by Jerard Austin
+ */
+
+import javax.swing.JOptionPane;
+import java.util.Scanner; 
+
+public class Apartment {
+    String address;
+    int numBedrooms;
+    int numBathrooms;
+    double price;
+
+
+    public Apartment(String address, int numBathrooms, int numBedrooms, double price){
+
+    }
+
+}
+
