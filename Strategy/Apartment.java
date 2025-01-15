004d5e5aeac0dcf1e005eead3d9bd64338abf68a
CSCE-JerardA <jerardaustin4@gmail.com>
2025-01-15 12:51:52 -0500

    apmnt constructor changes

--- Strategy/Apartment.java
+++ Strategy/Apartment.java
@@ -20,12 +20,23 @@ public class Apartment {
         this.numBathrooms = 0;
         this.numBedrooms = 0;
         this.price = 0.0;
-
-        
-        
     }
 
-    
+    public String getAddress(){
+        return address;
+    }
+
+    public int getNumBedrooms(){
+        return numBedrooms;
+    }
+
+    public int getNumBathrooms(){
+        return numBathrooms;
+    }
+
+    public double getPrice(){
+        return price;
+    }
 
 
 }
--- Strategy/ApartmentList.java
+++ Strategy/ApartmentList.java
@@ -1,5 +1,28 @@
 package Strategy;
 
+import java.util.ArrayList;
+
+/*
+ * Written by Jerard Austin
+ */
+
+
 public class ApartmentList {
-    
+    private ArrayList<Apartment> apartments;
+    private SortBehavior sortBehavior;
+
+    public ApartmentList(){
+        this.apartments = new ArrayList<>();
+    }
+
+    public void add(String address, int numBathrooms, int numBedrooms, double price){
+
+    }
+
+    public void setSortBehavior(){
+
+    }
+
+    public 
+
 }
