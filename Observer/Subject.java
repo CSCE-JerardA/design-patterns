58bd72bee88d8d00ba84575392793616ba752816
CSCE-JerardA <jerardaustin4@gmail.com>
2025-01-21 18:41:27 -0500

    adding to subject

--- Observer/Subject.java
+++ Observer/Subject.java
@@ -1,10 +1,31 @@
 package Observer;
+
 /*
  * @author Jerard Austin
  */
 
 
-public interface Subject implements Observer{
+public interface Subject{
     
+    /**
+     * @param observer
+     */
+    public void registerObserver(Observer observer){
+
+    }
+
+    /**
+     * @param observer
+     */
+    public void removeObserver(Observer observer){
+
+    }
+
+    public void notifyObservers(){
+
+    }
+
+
+
 
 }
