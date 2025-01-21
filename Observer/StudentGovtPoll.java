2addda00f88a5649ecc72e27340de055ca7fd8d3
CSCE-JerardA <jerardaustin4@gmail.com>
2025-01-21 18:37:57 -0500

    adding more

--- Observer/StudentGovtPoll.java
+++ Observer/StudentGovtPoll.java
@@ -10,5 +10,44 @@ import java.util.ArrayList;
 public class StudentGovtPoll{
 
     private ArrayList<Observer> observers;
+    private ArrayList<Candidate> candidates;
+
+    String school;
+
+    public StudentGovtPoll(String school){
+
+
+    }
+
+    public void registerObserver(Observer observer){
+
+    }
+
+    public void removeObserver(Observer observer){
+
+
+    }
+
+    public void notifyObservers(){
+
+
+    }
+
+    public void addCandidate(String firstName, String lastName ){
+
+
+    }
+
+    public void enterVotes(int firstPlace, int secondPlace, int thirdPlace){
+
+    }
+
+    public String getSchool(String school){
+        return school;
+    }
+
+
+
+
 
 }
