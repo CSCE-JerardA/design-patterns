072bb442a025caad1bc281f77e280907cf868cd1
CSCE-JerardA <jerardaustin4@gmail.com>
2025-01-29 12:10:02 -0500

    start of new design pattern

---
+++ PasswordProj/Password.java
@@ -0,0 +1,16 @@
+package PasswordProj;
+
+/*
+ * @author Jerard
+ */
+
+public class Password {
+
+    String password;
+    
+
+    public void getPassword(){
+
+    }
+
+}
---
+++ PasswordProj/PasswordDecorator.java
@@ -0,0 +1,13 @@
+package PasswordProj;
+
+/*
+ * @author Jerard Austin
+ */
+
+public class PasswordDecorator extends Password {
+    
+    Password passwordBeginning;
+
+    
+
+}
