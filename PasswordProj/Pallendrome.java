4dd082c759cdf684d927a914353bf462787857f9
CSCE-JerardA <jerardaustin4@gmail.com>
2025-02-02 19:03:42 -0500

    output

--- PasswordProj/Pallendrome.java
+++ PasswordProj/Pallendrome.java
@@ -1,11 +1,23 @@
 package PasswordProj;
 
+/*
+ * @author Jerard Austin
+ */
+
 public class Pallendrome extends PasswordDecorator{
 
+    /**
+     * Constructor that initializes the palindrome decorator.
+     * @param passwordBeginning The original password object.
+     */
     public Pallendrome(Password passwordBeginning) {
         super(passwordBeginning);
     }
 
+    /**
+     * Returns the password as a palindrome (original + reversed).
+     * @return The transformed palindrome password.
+     */
     @Override
     public String getPassword() {
         String original =passwordBeginning.getPassword();
