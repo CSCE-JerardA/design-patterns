ad33c18a15bc61ac4ea194e89ac119ff00478251
CSCE-JerardA <jerardaustin4@gmail.com>
2025-02-07 16:26:16 -0500

    MusicBox done

--- SongFolder/MusicBox.java
+++ SongFolder/MusicBox.java
@@ -1,43 +1,54 @@
 package SongFolder;
 
+/*
+ * @author Jerard Austin
+ */
 import java.util.ArrayList;
 
 public class MusicBox {
 
+    /*
+     * Variables making the different languaged states private
+     */
     private State state;
     private State englishState;
     private State frenchState;
     private State spanishState; 
 
-    public MusicBox(){
 
+    public MusicBox(){
+        this.englishState = englishState;
+        this.frenchState = frenchState;
+        this.spanishState = spanishState;
+
+        this.state = englishState;
     }
 
 
     public void pressStarButton(){
-
+        state.pressStarButton();
     }
 
     public void pressHappyButton(){
-
+        state.pressHappyButton();
     }
 
 
     public void pressEnglishButton(){
-
+        setState(englishState);
     }
 
     public void pressFrenchButton(){
-
+        setState(frenchState);
     }
 
 
     public void pressSpanishButton(){
-
+        setState(spanishState);
     }
 
-    public void setState(){
-
+    public void setState(State state){
+        this.state = state;
     }
 
     public State getEnglishState(){
@@ -53,7 +64,10 @@ public class MusicBox {
     }
 
     public void playSong(String songName, ArrayList<String> lyrics){
-
+        System.out.println("Play song: " + songName);
+        for(String lines : lyrics){
+            System.out.println(lines);
+        }
     }
 
 }
