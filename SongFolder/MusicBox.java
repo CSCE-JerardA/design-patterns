ee456dde3ed712f355908e0b78bce305af1a7c9b
CSCE-JerardA <jerardaustin4@gmail.com>
2025-02-07 11:11:10 -0500

    starting project

---
+++ SongFolder/MusicBox.java
@@ -0,0 +1,57 @@
+package SongFolder;
+
+public class MusicBox {
+
+    private State state;
+    private State englishState;
+    private State frenchState;
+    private State spanishState; 
+
+    public MusicBox(){
+
+    }
+
+
+    public void pressStarButton(){
+
+    }
+
+    public void pressHappyButton(){
+
+    }
+
+
+    public void pressEnglishButton(){
+
+    }
+
+    public void pressFrenchButton(){
+
+    }
+
+
+    public void pressSpanishButton(){
+
+    }
+
+    public void setState(){
+
+    }
+
+    public State getEnglishState(){
+        return englishState;
+    }
+
+    public State getFrenchState(){
+        return frenchState;
+    }
+
+    public State getSpanishState(){
+
+    }
+
+    public void playSong(String songName, ArrayList<String> lyrics){
+
+    }
+
+}
---
+++ SongFolder/State.java
@@ -0,0 +1,5 @@
+package SongFolder;
+
+public class State {
+    
+}
