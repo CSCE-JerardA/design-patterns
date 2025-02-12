e0a95b176a0fbb7f6f775a47ffc8f2e27e0b9d52
CSCE-JerardA <jerardaustin4@gmail.com>
2025-02-12 13:41:06 -0500

    creating more

---
+++ Boiler/Question.java
@@ -0,0 +1,35 @@
+package Boiler;
+
+/*
+ * @Author Jerard Austin
+ */
+
+public class Question {
+    
+    private int num1;
+    private int num2;
+    private int answer;
+    private int userAnswer;
+    private Operand operand;
+    
+    public Question(){
+
+    }
+
+    public String getQuestion(){
+
+    }
+
+    public void setUserAnwer(int answer){
+
+    }
+
+    public boolean isCorrect(){
+
+    }
+
+    public String toString(){
+
+    }
+    
+}
